f6ad26485305a12ddaed790144c30c3d78edefa8
bogia4848 <140590028+bogia4848@users.noreply.github.com>
2023-07-26 14:37:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'lihnnn  à!',
+    introDesc: `chiều hôm tỏ ra bóng nàng 
+    nhìn tia bóng chiếu qua hàng bây bay 
+    nàng ơi nàng liệu có hay 
+    mây bay nắng chiếu ta say 1 đời `,
+    btnIntro: 'say lắm rồi ',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'yêu anh ngay từ dầu <3',
+    btnNo: 'Không anh tán em chứ =))',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'tại vì thích anh nhiều quá nhưng anh nói À Lôi =)) <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messDesc: 'thế đồng ý làm ny anh nhé =)))',
+    btnAccept: 'dạ em đồng ý  <3',
+    messLink: ' //lihttps://www.facebook.com
